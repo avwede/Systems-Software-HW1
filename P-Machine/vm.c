24f8deaa5ce7e8bae3ddfc8368fbdcf1a108c279
NicholasCG <nicholascgray@knights.ucf.edu>
2021-09-15 19:50:16 -0400

    Added some functions from doc

--- P-Machine/vm.c
+++ P-Machine/vm.c
@@ -3,9 +3,46 @@
 
 #include <stdio.h>
 
+int fetch_instruction();
+int instruction_decode();
+int execute_instruction();
+
+int base(int L)
+{
+  int arb = BP;
+  while (L > 0)
+  {
+    arb = pas[arb];
+    L--;
+  }
+
+  return arb;
+}
+
+void print_execution(int line, char *opname, int *IR, int PC, int BP, int SP, int DP, int *pas, int GP)
+{
+  int i;
+
+  // print out instruction and registers
+  printf("%2d\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t", line, opname, IR[1], IR[2], PC, BP, SP, DP);
+
+  // print data section
+  for (i = GP; i <= DP; i++)
+    printf("%d ", pas[i]);
+
+  printf("\n");
+
+  // print stack
+  printf("\tstack : ");
+  for (i = MAX_PAS_LENGTH - 1; i >= SP; i--)
+    printf("%d ", pas[i]);
+  
+  printf("\n");
+}
+
 int main(void)
 {
-  printf("Hello, World!");
+  printf("Hello, World!\n");
   
   return 0;
 }
