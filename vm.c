5b3d34d6dc220d7d7b59ddbe567c800b078a2834
Nicholas Gray <70730217+NicholasCG@users.noreply.github.com>
2021-09-15 14:34:09 -0400

    Update vm.c

--- vm.c
+++ vm.c
@@ -1,2 +1,11 @@
 // Systems Software Fall 2021
 // Group 5 - Nicholas Gray, Abraham Hernandez, Prathik Ramesh, Ashley Voglewede
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, World!");
+  
+  return 0;
+}
