f8ba269a73cb351228a3e8af326ebace5d5354ce
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-10-14 15:45:43 -0400

    Initialize lexeme array

--- Lexer/lex.c
+++ Lexer/lex.c
@@ -1,14 +1,13 @@
-/* 
+/*
 	This is the lex.c file for the UCF Fall 2021 Systems Software Project.
 	For HW2, you must implement the function lexeme *lexanalyzer(char *input).
 	You may add as many constants, global variables, and support functions
 	as you desire.
-	
-	If you choose to alter the printing functions or delete list or lex_index, 
-	you MUST make a note of that in you readme file, otherwise you will lose 
+
+	If you choose to alter the printing functions or delete list or lex_index,
+	you MUST make a note of that in you readme file, otherwise you will lose
 	5 points.
 */
-
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -25,12 +24,15 @@ int lex_index;
 void printlexerror(int type);
 void printtokens();
 
-
 lexeme *lexanalyzer(char *input)
 {
-	return NULL;
-}
+	// Initialize lexeme array
+	list = (lexeme *)calloc(MAX_NUMBER_TOKENS, sizeof(lexeme));
 
+	// TODO: Implement Lexing
+
+	return list;
+}
 
 void printtokens()
 {
@@ -41,105 +43,105 @@ void printtokens()
 	{
 		switch (list[i].type)
 		{
-			case oddsym:
-				printf("%11s\t%d", "odd", oddsym);
-				break;
-			case eqlsym:
-				printf("%11s\t%d", "==", eqlsym);
-				break;
-			case neqsym:
-				printf("%11s\t%d", "!=", neqsym);
-				break;
-			case lsssym:
-				printf("%11s\t%d", "<", lsssym);
-				break;
-			case leqsym:
-				printf("%11s\t%d", "<=", leqsym);
-				break;
-			case gtrsym:
-				printf("%11s\t%d", ">", gtrsym);
-				break;
-			case geqsym:
-				printf("%11s\t%d", ">=", geqsym);
-				break;
-			case modsym:
-				printf("%11s\t%d", "%", modsym);
-				break;
-			case multsym:
-				printf("%11s\t%d", "*", multsym);
-				break;
-			case divsym:
-				printf("%11s\t%d", "/", divsym);
-				break;
-			case addsym:
-				printf("%11s\t%d", "+", addsym);
-				break;
-			case subsym:
-				printf("%11s\t%d", "-", subsym);
-				break;
-			case lparensym:
-				printf("%11s\t%d", "(", lparensym);
-				break;
-			case rparensym:
-				printf("%11s\t%d", ")", rparensym);
-				break;
-			case commasym:
-				printf("%11s\t%d", ",", commasym);
-				break;
-			case periodsym:
-				printf("%11s\t%d", ".", periodsym);
-				break;
-			case semicolonsym:
-				printf("%11s\t%d", ";", semicolonsym);
-				break;
-			case assignsym:
-				printf("%11s\t%d", ":=", assignsym);
-				break;
-			case beginsym:
-				printf("%11s\t%d", "begin", beginsym);
-				break;
-			case endsym:
-				printf("%11s\t%d", "end", endsym);
-				break;
-			case ifsym:
-				printf("%11s\t%d", "if", ifsym);
-				break;
-			case thensym:
-				printf("%11s\t%d", "then", thensym);
-				break;
-			case elsesym:
-				printf("%11s\t%d", "else", elsesym);
-				break;
-			case whilesym:
-				printf("%11s\t%d", "while", whilesym);
-				break;
-			case dosym:
-				printf("%11s\t%d", "do", dosym);
-				break;
-			case callsym:
-				printf("%11s\t%d", "call", callsym);
-				break;
-			case writesym:
-				printf("%11s\t%d", "write", writesym);
-				break;
-			case readsym:
-				printf("%11s\t%d", "read", readsym);
-				break;
-			case constsym:
-				printf("%11s\t%d", "const", constsym);
-				break;
-			case varsym:
-				printf("%11s\t%d", "var", varsym);
-				break;
-			case procsym:
-				printf("%11s\t%d", "procedure", procsym);
-				break;
-			case identsym:
-				printf("%11s\t%d", list[i].name, identsym);
-				break;
-			case numbersym:
-				printf("%11d\t%d", list[i].value, numbersym);
-				break;
+		case oddsym:
+			printf("%11s\t%d", "odd", oddsym);
+			break;
+		case eqlsym:
+			printf("%11s\t%d", "==", eqlsym);
+			break;
+		case neqsym:
+			printf("%11s\t%d", "!=", neqsym);
+			break;
+		case lsssym:
+			printf("%11s\t%d", "<", lsssym);
+			break;
+		case leqsym:
+			printf("%11s\t%d", "<=", leqsym);
+			break;
+		case gtrsym:
+			printf("%11s\t%d", ">", gtrsym);
+			break;
+		case geqsym:
+			printf("%11s\t%d", ">=", geqsym);
+			break;
+		case modsym:
+			printf("%11s\t%d", "%", modsym);
+			break;
+		case multsym:
+			printf("%11s\t%d", "*", multsym);
+			break;
+		case divsym:
+			printf("%11s\t%d", "/", divsym);
+			break;
+		case addsym:
+			printf("%11s\t%d", "+", addsym);
+			break;
+		case subsym:
+			printf("%11s\t%d", "-", subsym);
+			break;
+		case lparensym:
+			printf("%11s\t%d", "(", lparensym);
+			break;
+		case rparensym:
+			printf("%11s\t%d", ")", rparensym);
+			break;
+		case commasym:
+			printf("%11s\t%d", ",", commasym);
+			break;
+		case periodsym:
+			printf("%11s\t%d", ".", periodsym);
+			break;
+		case semicolonsym:
+			printf("%11s\t%d", ";", semicolonsym);
+			break;
+		case assignsym:
+			printf("%11s\t%d", ":=", assignsym);
+			break;
+		case beginsym:
+			printf("%11s\t%d", "begin", beginsym);
+			break;
+		case endsym:
+			printf("%11s\t%d", "end", endsym);
+			break;
+		case ifsym:
+			printf("%11s\t%d", "if", ifsym);
+			break;
+		case thensym:
+			printf("%11s\t%d", "then", thensym);
+			break;
+		case elsesym:
+			printf("%11s\t%d", "else", elsesym);
+			break;
+		case whilesym:
+			printf("%11s\t%d", "while", whilesym);
+			break;
+		case dosym:
+			printf("%11s\t%d", "do", dosym);
+			break;
+		case callsym:
+			printf("%11s\t%d", "call", callsym);
+			break;
+		case writesym:
+			printf("%11s\t%d", "write", writesym);
+			break;
+		case readsym:
+			printf("%11s\t%d", "read", readsym);
+			break;
+		case constsym:
+			printf("%11s\t%d", "const", constsym);
+			break;
+		case varsym:
+			printf("%11s\t%d", "var", varsym);
+			break;
+		case procsym:
+			printf("%11s\t%d", "procedure", procsym);
+			break;
+		case identsym:
+			printf("%11s\t%d", list[i].name, identsym);
+			break;
+		case numbersym:
+			printf("%11d\t%d", list[i].value, numbersym);
+			break;
 		}
 		printf("\n");
 	}
@@ -170,7 +172,7 @@ void printlexerror(int type)
 		printf("Lexical Analyzer Error: Excessive Identifier Length\n");
 	else
 		printf("Implementation Error: Unrecognized Error Type\n");
-	
+
 	free(list);
 	return;
 }
